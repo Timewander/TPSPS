030ccd15ce0e5aa0d0faec9a33af5545cf588053
Timewander <sky.gao@d1m.cn>
2017-09-06 14:11:01 +0800

    update server domain

--- Init/config.php
+++ Init/config.php
@@ -1,11 +1,11 @@
 <?php
 
 define("TMP_DIR", ROOT_DIR . '\..\tmp');
-define("POWER", "http://weboutique-sky.richemont.d1m.cn/switch");
+define("POWER", "http://proxy-sky.richemont.d1m.cn/power");
 define("SWITCH_STATUS", false);
 // choose log the request & response to disk or not
 // define("ACTION", "dealRequest");
 define("ACTION", "setRequest");
-define("REQUEST", "http://weboutique-sky.richemont.d1m.cn/request");
-define("RESPONSE", "http://weboutique-sky.richemont.d1m.cn/response");
+define("REQUEST", "http://proxy-sky.richemont.d1m.cn/request");
+define("RESPONSE", "http://proxy-sky.richemont.d1m.cn/response");
 define("DEAL", "http://localhost/client.php?action=" . ACTION);
